64c9e554b104709dedc7537ad6a2716c90349ab0
ennerlopez <ennerlopezz@gmail.com>
2015-08-18 14:44:19 -0600

    se cambio a directivas

--- public/Lib/Example1.js
+++ public/Lib/Example1.js
@@ -1,66 +1,65 @@
 (function (angular) {
     'use strict';
     var app = angular.module('Example1', []);
-    app.controller('MyController', ['$scope','$http', function ($scope,$http) {
+    app.controller('MyController', ['$scope', '$http', function ($scope, $http) {
 
         $http.get('formulario2.json')
-            .then(function(res){
+            .then(function (res) {
                 $scope.formulario = res.data;
             });
-       /* $scope.formulario = {
+        /* $scope.formulario = {
 
-            secciones: [
-                {
-                    codigo: 1,
-                    componentes: [
-                        {
-                            tipo: 'password',
-                            codigo: 1,
-                            nombre: 'Casilla',
-                            valor: ''
+         secciones: [
+         {
+         codigo: 1,
+         componentes: [
+         {
+         tipo: 'password',
+         codigo: 1,
+         nombre: 'Casilla',
+         valor: ''
 
-                        },
-                        {
-                            tipo: 'texto',
-                            codigo: 1,
-                            nombre: 'precio',
-                            valor: 'Texto'
+         },
+         {
+         tipo: 'texto',
+         codigo: 1,
+         nombre: 'precio',
+         valor: 'Texto'
 
-                        }]
-                },
-                {
-                    codigo: 2,
-                    componentes: [
-                        {
-                            tipo: 'password',
-                            codigo: 22,
-                            nombre: 'Casilla22',
-                            valor: '44444'
+         }]
+         },
+         {
+         codigo: 2,
+         componentes: [
+         {
+         tipo: 'password',
+         codigo: 22,
+         nombre: 'Casilla22',
+         valor: '44444'
 
-                        },
-                        {
-                            tipo: 'texto',
-                            codigo: 1,
-                            nombre: 'precio2',
-                            valor: 'Texto'
+         },
+         {
+         tipo: 'texto',
+         codigo: 1,
+         nombre: 'precio2',
+         valor: 'Texto'
 
-                        }]
-                },
-                {
-                    codigo: 3,
-                    componentes: [
-                        {
-                            tipo: 'texto',
-                            codigo: 333,
-                            nombre: 'precio2',
-                            valor: 'Texto'
+         }]
+         },
+         {
+         codigo: 3,
+         componentes: [
+         {
+         tipo: 'texto',
+         codigo: 333,
+         nombre: 'precio2',
+         valor: 'Texto'
 
-                        }]
-                }
-            ]
+         }]
+         }
+         ]
 
-        };*/
-
+         };*/
 
 
     }]);
@@ -69,7 +68,7 @@
 
         return {
 
-
+            replace: true,
             templateUrl: function () {
                 return 'componente-secciones.html'
             }
@@ -84,11 +83,13 @@
     app.directive('componenteSeccion', function () {
 
         return {
+         /*   replace: true,
+            template: '<div ng-repeat = "component in seccion.componentes">' +
+            '<ng-include src="&quot;componente-&quot;+component.tipo+&quot;.html&quot;"></ng-include>' +
+                ' </div> '
+           */
 
-
-            templateUrl: function () {
-                return 'componente-seccion.html'
-            }
+             templateUrl: 'componente-seccion.html'
 
 
         }
@@ -96,11 +97,25 @@
 
     });
 
+    app.directive('componenteX', function () {
+
+        return {
+            templateUrl: function(elem,attr){
+                return 'componente-'+ attr.type+'.html'
+            }
+
+
+        }
+
+
+    })
+
+
     app.directive('componenteTexto', function () {
 
         return {
-
-            template : '|{{component.codigo}} - {{component.nombre}}|<input type="text" ng-model="component.valor">'
+            replace: true,
+            template: '|{{component.codigo}} - {{component.nombre}}|<input type="text" ng-model="component.valor">'
 
 
         }
