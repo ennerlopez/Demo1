bf0d0d9276c8e00502772f6ceab6b658cbf47fff
ennerlopez <ennerlopezz@gmail.com>
2015-08-19 17:35:55 -0600

    Avance aplicacion diseño

--- public/Lib/Example1.js
+++ public/Lib/Example1.js
@@ -3,63 +3,78 @@
     var app = angular.module('Example1', []);
     app.controller('MyController', ['$scope', '$http', function ($scope, $http) {
 
-        $http.get('formulario.json')
-            .then(function (res) {
-                $scope.formulario = res.data;
-            });
-        /* $scope.formulario = {
+        //$http.get('formulario.json')
+        //    .then(function (res) {
+        //        $scope.formulario = res.data;
+        //    });
+        $scope.formulario = {
 
-         secciones: [
-         {
-         codigo: 1,
-         componentes: [
-         {
-         tipo: 'password',
-         codigo: 1,
-         nombre: 'Casilla',
-         valor: ''
+            secciones: [
+                {
+                    codigo: 1,
+                    filas: [
 
-         },
-         {
-         tipo: 'texto',
-         codigo: 1,
-         nombre: 'precio',
-         valor: 'Texto'
+                        {
 
-         }]
-         },
-         {
-         codigo: 2,
-         componentes: [
-         {
-         tipo: 'password',
-         codigo: 22,
-         nombre: 'Casilla22',
-         valor: '44444'
+                            componentes: [
+                                {
+                                    tipo: 'password',
+                                    codigo: 1,
+                                    nombre: '[1,1]',
+                                    valor: ''
 
-         },
-         {
-         tipo: 'texto',
-         codigo: 1,
-         nombre: 'precio2',
-         valor: 'Texto'
+                                },
+                                {
+                                    tipo: 'texto',
+                                    codigo: 2,
+                                    nombre: '[1,2]',
+                                    valor: 'Texto'
 
-         }]
-         },
-         {
-         codigo: 3,
-         componentes: [
-         {
-         tipo: 'texto',
-         codigo: 333,
-         nombre: 'precio2',
-         valor: 'Texto'
+                                },
+                                {
+                                    tipo: 'texto',
+                                    codigo: 2,
+                                    nombre: '[1,3]',
+                                    valor: 'Texto'
 
-         }]
-         }
-         ]
+                                }
 
-         };*/
+
+                            ]
+                        },
+                        {
+
+                            componentes: [
+                                {
+                                    tipo: 'password',
+                                    codigo: 3,
+                                    nombre: '[2,1]',
+                                    valor: ''
+
+                                },
+                                {
+                                    tipo: 'vacio',
+                                    codigo: 4,
+                                    nombre: '[2,2]',
+                                    valor: 'Texto'
+
+                                },
+                                {
+                                    tipo: 'password',
+                                    codigo: 4,
+                                    nombre: '[2,3]',
+                                    valor: ''
+
+                                }
+
+
+                            ]
+                        }
+
+                    ]
+                }]
+
+        };
 
 
     }]);
@@ -83,13 +98,13 @@
     app.directive('componenteSeccion', function () {
 
         return {
-            replace: true,
-         /*   template: '<div ng-repeat = "component in seccion.componentes">' +
-            '<ng-include src="&quot;componente-&quot;+component.tipo+&quot;.html&quot;"></ng-include>' +
-                ' </div> '
-           */
+            //replace: true,
+            /*   template: '<div ng-repeat = "component in seccion.componentes">' +
+             '<ng-include src="&quot;componente-&quot;+component.tipo+&quot;.html&quot;"></ng-include>' +
+             ' </div> '
+             */
 
-             templateUrl: 'componente-seccion.html'
+            templateUrl: 'componente-seccion.html'
 
 
         }
@@ -100,8 +115,8 @@
     app.directive('componenteX', function () {
 
         return {
-            templateUrl: function(elem,attr){
-                return 'componente-'+ attr.type+'.html'
+            templateUrl: function (elem, attr) {
+                return 'componente-' + attr.type + '.html'
             }
 
 
@@ -116,6 +131,18 @@
         return {
             replace: true,
             template: '|{{component.codigo}} - {{component.nombre}}|<input type="text" ng-model="component.valor">'
+
+
+        }
+
+
+    });
+
+    app.directive('componenteFila', function () {
+
+        return {
+
+            templateUrl: 'componente-fila.html'
 
 
         }
